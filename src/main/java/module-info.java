80def5197ba05a597ade17fdde6a305c375b37ec
rzngnam1402 <legiangnam1402@gmail.com>
2023-01-31 20:59:13 +0700

    add javafx dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,12 @@ module com.example.ui {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
+//    requires org.controlsfx.controls;
+//    requires org.kordamp.bootstrapfx.core;
+
+    requires org.jsoup;
+
+    requires com.google.gson;
 
     opens com.example.ui to javafx.fxml;
     exports com.example.ui;
