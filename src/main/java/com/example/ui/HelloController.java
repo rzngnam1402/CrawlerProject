5b97a7ee8423a8e221ad7552f3ebccbf4f63d508
minh nguyen <nguyenducm3@gmail.com>
2023-01-31 10:04:30 +0700

    UI_Minh from local merge to remote project

---
+++ src/main/java/com/example/ui/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    protected List<String> searchList(String searchWords, List<String> listOfStrings) {
+
+        List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
+
+        return listOfStrings.stream().filter(input -> {
+            return searchWordsArray.stream().allMatch(word ->
+                    input.toLowerCase().contains(word.toLowerCase()));
+        }).collect(Collectors.toList());
+    }
+}
